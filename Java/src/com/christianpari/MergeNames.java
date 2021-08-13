fee91cd804a383f6aa6661b814420daa734482de
Christian Pari <christianpari@outlook.com>
2021-08-13 18:51:00 -0400

    Using given arrays to convert into a Set of unique values and returns as a String

---
+++ Java/src/com/christianpari/MergeNames.java
@@ -0,0 +1,20 @@
+package com.christianpari;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class MergeNames {
+  public static String[] uniqueNames(String[] names1, String[] names2) {
+    HashSet<String> names = new HashSet<>();
+    names.addAll(Arrays.asList(names1));
+    names.addAll(Arrays.asList(names2));
+    return names.toArray(new String[0]);
+  }
+
+  public static void main(String[] args) {
+    String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+    String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+    System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2)));
+    // should print Ava, Emma, Olivia, Sophia
+  }
+}
